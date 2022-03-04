c2f4662258f8d15ac13a9bfd766d229c226edd06
clonomaer <clonomaer@gmail.com>
2022-03-04 21:14:25 +0330

    add default blocks confirmation

--- utils/make-transaction-response.ts
+++ utils/make-transaction-response.ts
@@ -1,10 +1,13 @@
 import { TransactionResponse as BaseTransactionResponse } from "@ethersproject/providers";
+import { config } from "../configs";
 import { TransactionResponse } from "../types/web3";
 
 export function makeTransactionResponse(
   input: Promise<BaseTransactionResponse>
 ): TransactionResponse {
   (input as TransactionResponse).wait = (confirmations) =>
-    input.then((response) => response.wait(confirmations));
+    input.then((response) =>
+      response.wait(confirmations ?? config.BlocksToWaitForConfirmation)
+    );
   return input as TransactionResponse;
 }
