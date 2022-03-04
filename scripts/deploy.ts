3057c09167423b132abc75594f0830ef5d546760
clonomaer <clonomaer@gmail.com>
2022-03-05 00:00:48 +0330

    better deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,7 @@
 import hre from "hardhat";
+import { readFile, writeFile, mkdir, stat } from "fs/promises";
+import path from "path";
+
 async function main() {
   const [deployer, ...rest] = await hre.ethers.getSigners();
   if (deployer === undefined) {
@@ -7,9 +10,47 @@ async function main() {
   console.log("Deploying contracts with the account: ", deployer.address);
   console.log("Account balance: ", (await deployer.getBalance()).toString());
 
-  const IPBlogV1Factory = await hre.ethers.getContractFactory("IPBlogV1");
+  const contractKey = "IPBlogV1";
+
+  const IPBlogV1Factory = await hre.ethers.getContractFactory(contractKey);
   const ipblogV1 = await IPBlogV1Factory.deploy();
   console.log("Deployed successfully at: ", ipblogV1.address);
+
+  const abi = JSON.parse(
+    (
+      await readFile(
+        path.join(
+          __dirname,
+          "..",
+          "artifacts",
+          "contracts",
+          `${contractKey}.sol`,
+          `${contractKey}.json`
+        )
+      )
+    ).toString()
+  ).abi;
+  try {
+    await stat(path.join(__dirname, "..", "deployments"));
+  } catch {
+    await mkdir(path.join(__dirname, "..", "deployments"));
+  }
+  try {
+    await stat(path.join(__dirname, "..", "deployments", hre.network.name));
+  } catch {
+    await mkdir(path.join(__dirname, "..", "deployments", hre.network.name));
+  }
+  await writeFile(
+    path.join(
+      __dirname,
+      "..",
+      "deployments",
+      hre.network.name,
+      `${contractKey}.json`
+    ),
+    JSON.stringify({ address: ipblogV1.address, abi })
+  );
+  console.log("Deployment info successfully written!");
 }
 main()
   .then(() => process.exit(0))
